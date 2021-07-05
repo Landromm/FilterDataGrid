8c2f0b53c32ec5af5a6a4e159e57cf47b80b91c8
Igor Radkevich <landrommigor@gmail.com>
2021-07-05 22:18:58 +0300

    Добавьте файлы проекта.

---
+++ PLCTable.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilterDataGrid
+{
+    class PLCTable
+    {
+        public PLCTable(int id, string name, string fabric, string model, int countModul, string ip, string district, string typeOfObject, string numberOfObject, string place, string typeBoard, int number, string comment)
+        {
+            Id = id;
+            Name = name;
+            Fabric = fabric;
+            Model = model;
+            CountModul = countModul;
+            Ip = ip;
+            District = district;
+            TypeOfObject = typeOfObject;
+            NumberOfObject = numberOfObject;
+            Place = place;
+            TypeBoard = typeBoard;
+            Number = number;
+            Comment = comment;
+        }
+
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Fabric { get; private set; }
+        public string Model { get; private set; }
+        public int CountModul { get; private set; }
+        public string Ip { get; private set; }
+        public string District { get; private set; }
+        public string TypeOfObject { get; private set; }
+        public string NumberOfObject { get; private set; }
+        public string Place { get; private set; }
+        public string TypeBoard { get; private set; }
+        public int Number { get; private set; }
+        public string Comment { get; private set; }
+    }
+}
